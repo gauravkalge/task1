092f730666526b26bb735527968a598ec7e2a1a0
kalge <42073937+kalge@users.noreply.github.com>
2022-01-11 17:16:35 +0530

    with error

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:form_field_validator/form_field_validator.dart';
 import 'package:task1/utils/routes.dart';
+import 'package:task1/widgets/botton_widget.dart';
 import 'package:task1/widgets/widgets.dart';
 
 class Signup extends StatefulWidget {
@@ -11,6 +13,11 @@ class Signup extends StatefulWidget {
 }
 
 class _SignupState extends State<Signup> {
+  late String name, email, phone;
+  //TextController to read text entered in text field
+  TextEditingController password = TextEditingController();
+  TextEditingController confirmpassword = TextEditingController();
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,131 +30,167 @@ class _SignupState extends State<Signup> {
         child: Column(
           children: [
             title(context),
-            // ignore: prefer_const_constructors
-            Text(
+            const Text(
               "welcome to Family !!!",
-              style: const TextStyle(
+              style: TextStyle(
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
                   color: Colors.blue),
             ),
-            Text(
+            const Text(
               'Register your account',
               style: TextStyle(fontWeight: FontWeight.bold),
             ),
             Padding(
               padding:
                   const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Company Name',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: 'xyz abcd',
-                      prefixIcon: Icon(Icons.person),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15.0)),
+              child: Form(
+                key: signupformKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Company Name',
+                      style: TextStyle(fontSize: 16),
                     ),
-                    keyboardType: TextInputType.emailAddress,
-                    textInputAction: TextInputAction.done,
-                    autofocus: true,
-                  ),
-                  Text(
-                    'Email Id',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: 'name@example.com',
-                      prefixIcon: Icon(Icons.person),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15.0)),
+                    TextFormField(
+                      //autovalidateMode: AutovalidateMode.onUserInteraction,
+                      decoration: InputDecoration(
+                        hintText: 'xyz abcd',
+                        prefixIcon: Icon(Icons.person),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15.0)),
+                      ),
+                      validator: RequiredValidator(
+                          errorText: 'This field can\'t be null'),
+                      keyboardType: TextInputType.emailAddress,
+                      textInputAction: TextInputAction.done,
+                      autofocus: true,
                     ),
-                    keyboardType: TextInputType.emailAddress,
-                    textInputAction: TextInputAction.done,
-                    autofocus: true,
-                  ),
-                  Text(
-                    'Mobile No',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: '90*********',
-                      prefixIcon: Icon(Icons.person),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15.0)),
+                    const Text(
+                      'Email Id',
+                      style: TextStyle(fontSize: 16),
                     ),
-                    keyboardType: TextInputType.emailAddress,
-                    textInputAction: TextInputAction.done,
-                    autofocus: true,
-                  ),
-                  Text(
-                    'Password',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: 'Password',
-                      // errorText: 'Password is wrong',
-                      prefixIcon: Icon(Icons.lock),
-                      suffixIcon: Icon(Icons.visibility),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'name@example.com',
+                        prefixIcon: Icon(Icons.person),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15.0)),
+                      ),
+                      validator: MultiValidator([
+                        EmailValidator(errorText: 'enter proper mail address'),
+                        RequiredValidator(
+                            errorText: 'This field can\'t be null')
+                      ]),
+                      keyboardType: TextInputType.emailAddress,
+                      textInputAction: TextInputAction.done,
+                      autofocus: true,
+                    ),
+                    const Text(
+                      'Mobile No',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: '90*********',
+                        prefixIcon: Icon(Icons.person),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15.0)),
+                      ),
+                      validator: MultiValidator([
+                        RangeValidator(
+                            min: 10,
+                            max: 10,
+                            errorText: 'mobile number should be 10 digits')
+                      ]),
+                      keyboardType: TextInputType.emailAddress,
+                      textInputAction: TextInputAction.done,
+                      autofocus: true,
+                    ),
+                    const Text(
+                      'Password',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    TextFormField(
+                      controller: password,
+                      validator: MultiValidator([
+                        RequiredValidator(errorText: "enter valid password"),
+                        PatternValidator(
+                            '^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}',
+                            errorText:
+                                'Minimum eight characters, at least one letter and one number')
+                      ]),
+                      decoration: InputDecoration(
+                        hintText: 'Password',
+                        // errorText: 'Password is wrong',
+                        prefixIcon: Icon(Icons.lock),
+                        suffixIcon: Icon(Icons.visibility),
 
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15.0)),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15.0)),
+                      ),
                     ),
-                  ),
-                  Text(
-                    'Confirm Password',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: 'Password',
-                      // errorText: 'Password is wrong',
-                      prefixIcon: Icon(Icons.lock),
-                      suffixIcon: Icon(Icons.visibility),
+                    const Text(
+                      'Confirm Password',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    TextFormField(
+                      controller: confirmpassword,
+                      validator: (value) {
+                        if (value == '') {
+                          return 'Please re-enter password';
+                        }
+                        print(password.text);
+                        print(confirmpassword.text);
+                        if (password.text != confirmpassword.text) {
+                          return "Password does not match";
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        hintText: 'Password',
+                        // errorText: 'Password is wrong',
+                        prefixIcon: Icon(Icons.lock),
+                        suffixIcon: Icon(Icons.visibility),
 
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15.0)),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15.0)),
+                      ),
                     ),
-                  ),
-                  Row(
-                    children: [
-                      Material(
-                        child: Checkbox(
-                          value: true,
-                          onChanged: (value) {},
+                    Row(
+                      children: [
+                        Material(
+                          child: Checkbox(
+                            value: true,
+                            onChanged: (value) {},
+                          ),
                         ),
-                      ),
-                      Text(
-                        'I have read and accept terms \nand conditions',
-                        overflow: TextOverflow.ellipsis,
-                      )
-                    ],
-                  ),
-                  Center(child: buildbutton(context, 'Sign-Up')),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("I have an accont?"),
-                      InkWell(
-                        child: Text('login'),
-                        onTap: () {
-                          Navigator.pushNamed(
-                            context,
-                            MyRoutes.loginRoute,
-                          );
-                        },
-                      ),
-                    ],
-                  ),
-                ],
+                        const Text(
+                          'I have read and accept terms \nand conditions',
+                          overflow: TextOverflow.ellipsis,
+                        )
+                      ],
+                    ),
+                    Center(child: buildButton(context)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text("I have an accont?"),
+                        InkWell(
+                          child: Text('login'),
+                          onTap: () {
+                            Navigator.pushNamed(
+                              context,
+                              MyRoutes.loginRoute,
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             )
           ],
@@ -156,3 +199,24 @@ class _SignupState extends State<Signup> {
     );
   }
 }
+
+Widget buildButton(context) => ButtonWidget(
+      text: 'SIGNUP',
+      onClicked: () {
+        final form = signupformKey.currentState!;
+        Navigator.pushNamed(
+          context,
+          MyRoutes.loginRoute,
+        );
+
+        if (form.validate()) {
+          final email = emailController.text;
+
+          ScaffoldMessenger.of(context)
+            ..removeCurrentSnackBar()
+            ..showSnackBar(const SnackBar(
+              content: Text('proccessing your data'),
+            ));
+        }
+      },
+    );
